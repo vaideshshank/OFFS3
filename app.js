9dc89bf794024f3a738919aae3b0b51ad50c19ce
Sajeev Mahajan <sjv97mhjn@gmail.com>
2017-11-17 00:17:59 +0530

    temporary commit

--- app.js
+++ app.js
@@ -42,10 +42,8 @@ app.use("/", routes);
 
 //middleware
 
-
-
-app.listen(8080,function() {
-	console.log("Listening On port " + 3000);
+app.listen(80,function() {
+	console.log("Listening On port " + 80);
 });
 
 //SQL LOCK TRANSACTION , CHECK IF INTERNALLY PRESENT.
