14db34f1db77d22f8ba561f72fd2b83c0dbf219f
riyabhatia26 <riyabhatia26@gmail.com>
2017-12-23 22:07:31 +0530

    silly mistakee corrected

--- models/mysql.js
+++ models/mysql.js
@@ -17,13 +17,13 @@ con.connect(function(err) {
 module.exports= con ;
 <<<<<<< HEAD
 */
-
+/*
 =======
 
 */
->>>>>>> teacher_module
+
 	// update usmc_feedback_2017 uncomment karna 
-// set at_1='0' ,at_2='0', at_3='0', at_4='0', at_5='0', at_6='0', at_7='0', at_8='0', at_9='0', at_10='0', at_11='0', at_12='0', at_13='0', at_14='0', at_15='0'
+/// set at_1='0' ,at_2='0', at_3='0', at_4='0', at_5='0', at_6='0', at_7='0', at_8='0', at_9='0', at_10='0', at_11='0', at_12='0', at_13='0', at_14='0', at_15='0'
 	var connection=require('express-myconnection');
 	var mysql=require('mysql');
 
