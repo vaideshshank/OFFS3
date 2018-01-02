3e399c031f4c37028456ab4fc7eaa813a380d7b2
thestormykid <hanugautam96@gmail.com>
2018-01-02 19:07:19 +0530

    frontend port changed

--- facultyFrontend/app/config.js
+++ facultyFrontend/app/config.js
@@ -1,7 +1,3 @@
 var BACKEND = 'http://14.139.60.214:80';
-//<<<<<<< HEAD
-//var  BACKEND = 'http://localhost:8080'
-//=======
 // var  BACKEND = 'http://localhost:8080'
-//>>>>>>> 724164fdb38381a7184d12d416756ec3cf1338f5
 // var FRONTEND = 'http://14.139.60.214:80';
