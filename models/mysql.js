956219fcf30e485663ea1302e953512dcf22ede9
Sajeev Mahajan <sjv97mhjn@gmail.com>
2018-04-16 13:50:58 +0530

    Student_Module_Modified

--- models/mysql.js
+++ models/mysql.js
@@ -2,27 +2,6 @@ var connection=require('express-myconnection');
 var mysql=require('mysql');
 
 
-<<<<<<< HEAD
-// //http:php.ashutosh.pw
-
-// 
-// var con=mysql.createConnection({
-//   host:"myshop.ctikxe6frwwu.ap-south-1.rds.amazonaws.com",
-//   user:"myshop",
-//   password:"aXpw5GPjJPCbyUzb",
-//   database:'sdc'
-// });
-// con.connect(function(err) {
-//   if(err)
-//     throw err;
-//   console.log("Connected");
-// });
-// module.exports= con;
-
-
-=======
->>>>>>> b7f3aca83d456380c4607b3fbf3758f65034a6c2
-
 //localhost phpmyadmin
 //
 var con = mysql.createConnection({
