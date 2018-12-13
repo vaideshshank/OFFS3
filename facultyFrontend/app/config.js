49bd008351cbb4362e25e2984324a4ea13b749f9
Divyansh Tripathi <divyansh2998@icloud.com>
2018-12-13 13:26:05 +0000

    Pre-major Bug Solving Sprint

--- facultyFrontend/app/config.js
+++ facultyFrontend/app/config.js
@@ -1,5 +1,5 @@
 // var BACKEND = 'http://169.14.139.60.220:80';
-var BACKEND = 'http://localhost:8080'
+var BACKEND = 'http://139.59.84.178' 
 // var BACKEND = 'http://localhost:80'
 // var BACKEND = 'http://14.139.60.214';
 // var BACKEND = 'http://usict.ggsipu.nic.in';
