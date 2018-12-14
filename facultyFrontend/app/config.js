391507caef55ab07bdbe37d236f8cdc9726b1ed0
Divyansh Tripathi <divyansh2998@icloud.com>
2018-12-14 11:09:33 +0000

    bugs related

--- facultyFrontend/app/config.js
+++ facultyFrontend/app/config.js
@@ -1,6 +1,6 @@
 // var BACKEND = 'http://169.14.139.60.220:80';
-// var BACKEND = 'http://139.59.84.178' 
-var BACKEND = 'http://localhost:8080'
+ var BACKEND = 'http://139.59.84.178' 
+// var BACKEND = 'http://localhost:8080'
 // var BACKEND = 'http://14.139.60.214';
 // var BACKEND = 'http://usict.ggsipu.nic.in';
 // var BACKEND = 'http://10.102.7.6';
