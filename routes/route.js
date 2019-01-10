a1919222fa6fb3c3f47330e9d71e21ef92498442
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2019-01-10 16:22:27 +0530

    resetPassword

--- routes/route.js
+++ routes/route.js
@@ -9,7 +9,8 @@ var routes = {
     pvc: require("./views/pvc"),
     teacher: require("./views/teacher"),
     dataportal: require("./views/dataportal"),
-    dataStatus: require("./views/dataStatus")
+    dataStatus: require("./views/dataStatus"),
+    resetPassword: require("./views/resetPassword")
   }
 };
 
@@ -31,42 +32,45 @@ router.get("/slogout", routes.views.index.logout);
 	Erase this after you understand !
 */
 
-router.post("/dinitials",routes.views.dean.initials);  //To authenticate the dean
-router.get("/dchecksession",routes.views.dean.checksession);  //This is a temporary route.
-router.get("/ddashboard",routes.views.dean.dashboard);    // To get details of a BACH .
-router.get("/dlogout",routes.views.dean.logout);
-router.post("/dupload_photo",routes.views.dean.upload_photo);
+router.get("/resetpassword/:resetvar", routes.views.resetPassword.reset);
+router.post("/resetpassword", routes.views.resetPassword.resetRequest);
 
-router.post("/vinitials",routes.views.vc.initials);  //To authenticate the dean
-router.get("/vchecksession",routes.views.vc.checksession);  //This is a temporary route.
-router.get("/vdashboard",routes.views.vc.dashboard);    // To get details of a BACH .
-router.get("/vlogout",routes.views.vc.logout);
-router.post("/vupload_photo",routes.views.vc.upload_photo);
+router.post("/dinitials", routes.views.dean.initials); //To authenticate the dean
+router.get("/dchecksession", routes.views.dean.checksession); //This is a temporary route.
+router.get("/ddashboard", routes.views.dean.dashboard); // To get details of a BACH .
+router.get("/dlogout", routes.views.dean.logout);
+router.post("/dupload_photo", routes.views.dean.upload_photo);
 
-router.post("/pvinitials",routes.views.pvc.initials);  //To authenticate the dean
-router.get("/pvchecksession",routes.views.pvc.checksession);  //This is a temporary route.
-router.get("/pvdashboard",routes.views.pvc.dashboard);    // To get details of a BACH .
-router.get("/pvclogout",routes.views.pvc.logout);
-router.post("/pvcupload_photo",routes.views.pvc.upload_photo);
+router.post("/vinitials", routes.views.vc.initials); //To authenticate the dean
+router.get("/vchecksession", routes.views.vc.checksession); //This is a temporary route.
+router.get("/vdashboard", routes.views.vc.dashboard); // To get details of a BACH .
+router.get("/vlogout", routes.views.vc.logout);
+router.post("/vupload_photo", routes.views.vc.upload_photo);
 
-router.post("/tinitials",routes.views.teacher.initials);  //To authenticate the teacher
-router.get("/tchecksession",routes.views.teacher.checksession);  //This is a temporary route: profile details
-router.get("/tpopulate",routes.views.teacher.populate);    // To populate the dropdowns
-router.get("/tdashboard",routes.views.teacher.dashboard);    // To view the feedback of a BACH .
-router.get("/tlogout",routes.views.teacher.logout);
-router.post("/tupload_photo",routes.views.teacher.upload_photo);
+router.post("/pvinitials", routes.views.pvc.initials); //To authenticate the dean
+router.get("/pvchecksession", routes.views.pvc.checksession); //This is a temporary route.
+router.get("/pvdashboard", routes.views.pvc.dashboard); // To get details of a BACH .
+router.get("/pvclogout", routes.views.pvc.logout);
+router.post("/pvcupload_photo", routes.views.pvc.upload_photo);
 
-router.get("/getTeacher",routes.views.dataportal.getTeacher);
-router.get('/getCourse', routes.views.dataportal.getCourse);
-router.get('/getStream', routes.views.dataportal.getStream);
-router.get('/getSubjects', routes.views.dataportal.getSubjects);
-router.post("/createFeedback",routes.views.dataportal.createFeedback);
+router.post("/tinitials", routes.views.teacher.initials); //To authenticate the teacher
+router.get("/tchecksession", routes.views.teacher.checksession); //This is a temporary route: profile details
+router.get("/tpopulate", routes.views.teacher.populate); // To populate the dropdowns
+router.get("/tdashboard", routes.views.teacher.dashboard); // To view the feedback of a BACH .
+router.get("/tlogout", routes.views.teacher.logout);
+router.post("/tupload_photo", routes.views.teacher.upload_photo);
+
+router.get("/getTeacher", routes.views.dataportal.getTeacher);
+router.get("/getCourse", routes.views.dataportal.getCourse);
+router.get("/getStream", routes.views.dataportal.getStream);
+router.get("/getSubjects", routes.views.dataportal.getSubjects);
+router.post("/createFeedback", routes.views.dataportal.createFeedback);
 
 router.post("/sendStudent", routes.views.index.studentData);
 
 router.get("/getSubjectStatus", routes.views.dataStatus.getSubjectStatus);
-router.get("/getSubjectStatusByCourse", routes.views.dataStatus.getSubjectStatusByCourse);
-router.get("/getSubjectStatusByStream", routes.views.dataStatus.getSubjectStatusByStream);
-router.get("/getSubjectStatusBySemester", routes.views.dataStatus.getSubjectStatusBySemester);
+// router.get("/getSubjectStatusByCourse", routes.views.dataStatus.getSubjectStatusByCourse);
+// router.get("/getSubjectStatusByStream", routes.views.dataStatus.getSubjectStatusByStream);
+// router.get("/getSubjectStatusBySemester", routes.views.dataStatus.getSubjectStatusBySemester);
 
 module.exports = router;
